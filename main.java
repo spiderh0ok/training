6848881b6dcd2fa8f3cfbf06380e921bbdff9485
hillelv <hillel.vider@gmail.com>
2025-08-03 17:55:41 +0300

    record:task6

---
+++ main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        Student rafael = new Student("Rafael",8,1);
+        System.out.println(rafael.toString());
+        System.out.println(rafael.isInTichon());
+    }
+}
