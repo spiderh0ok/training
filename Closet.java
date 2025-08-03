3075ff58710b93deafecd1c8f52d616757ae2d03
hillelv <hillel.vider@gmail.com>
2025-08-03 16:39:26 +0300

    task6-8

---
+++ Closet.java
@@ -0,0 +1,23 @@
+public class Closet {
+    private Shirt[] shirts;
+
+    public Closet(Shirt[] shirts){
+        this.shirts = shirts;
+    }
+
+    public Shirt[] getShirts() {
+        return shirts;
+    }
+
+    public Shirt[] getShirtsBySize(int size){
+        Shirt[] sizeMatch = {};
+        int emptyslot = 0;
+        for (int i = 0; i < shirts.length; i++) {
+            if (shirts[i].getSize()==size) {
+                sizeMatch[emptyslot] = shirts[i];
+                emptyslot++;
+            }
+        }
+        return sizeMatch;
+    }
+}
