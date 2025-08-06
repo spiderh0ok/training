7f0b0ee846bb596edf8977424cef9a771e0667a2
spiderh0ok <rafaelkeselman@gmail.com>
2025-08-06 20:21:38 +0300

    enum:mid6

---
+++ Products.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public enum Products {
+
+    CHEESE(5,"Cheese"),
+    WATER_BOTTLE(3,"Water bottle"),
+    OIL_BOTTLE(10,"Oil bottle"),
+    SUGAR(7,"Sugar"),
+    APPLE(3,"Apple");
+
+    private int price;
+    private String name;
+
+    Products(int price,String name){
+        this.price=price;
+        this.name=name;
+    }
+
+    public int price(int[] amounts,Products[] items){
+        int price=0;
+        for (int i = 0; i < items.length; i++) {
+            price+=
+
+        }
+
+    }
+
+
+
+
+
+
+}
