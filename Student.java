b1a6ace8a411c1e64b5532f53595a501d617c56f
spiderh0ok <rafaelkeselman@gmail.com>
2025-08-06 16:52:55 +0300

    fix:resolves

--- Student.java
+++ Student.java
@@ -1,16 +1,21 @@
 public record Student(String name,int grade,int classNum) {
-    @Override
+
+    public static final int DEFAULT_GRADE = 9;
+    public static final int GRADE_FOR_TICHON = 10;
+
+
+    public Student(String name,int classNum){
+        this(name,DEFAULT_GRADE,classNum);
+    }
+
+    public boolean isInTichon(){
+        return grade>=GRADE_FOR_TICHON;
+
+    }
     public String toString() {
         return "Student " + name + " is in grade " + grade + "th and in class " + classNum;
     }
-    public boolean isInTichon(){
-        if (grade>=10)
-            return true;
-        return false;
-    }
 
-    public Student(String name,int classNum){
-        this(name,9,classNum);
-    }
+
 
 }
