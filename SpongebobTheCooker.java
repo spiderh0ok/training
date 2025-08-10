ef43f5c9dbb147c0275cc14e89bb6a3cf151a424
spiderh0ok <rafaelkeselman@gmail.com>
2025-08-10 17:34:24 +0300

    inheretance:7

--- SpongebobTheCooker.java
+++ SpongebobTheCooker.java
@@ -2,4 +2,10 @@ public class SpongebobTheCooker extends Spongebob{
     public SpongebobTheCooker(String name, int age, String favFood) {
         super("bob", 12, "oil");
     }
+
+    @Override
+    public void playBasketBall() {
+        super.playBasketBall();
+        System.out.println("I hate basketball!");
+    }
 }
