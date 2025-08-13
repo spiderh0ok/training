d6c5df62e1e174a99f18c61be4fc6c650a48be26
spiderh0ok <rafaelkeselman@gmail.com>
2025-08-13 16:32:20 +0300

    generics:task2

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void print (Object[] arrayToPrint){
+        for (int i = 0; i < arrayToPrint.length; i++) {
+            System.out.println(arrayToPrint[i]);
+        }
+    }
+
+
+
+}
